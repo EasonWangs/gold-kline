4379d5879caa6b81b7665d50b2623283bc72de41
EasonWangs <neoxone2@gmail.com>
2025-09-20 14:30:20 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     port: 5081,
     host: '0.0.0.0',
     open: false,
+    allowedHosts: ['cssass.com', '127.0.0.1', 'localhost'],
     proxy: {
       '/api/gold': {
         target: 'http://127.0.0.1:5080',
